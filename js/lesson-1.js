fa232e7424fd05d7664e12d0c37ab987cb5a0374
Oleksii <eon.ulthar@icloud.com>
2025-05-22 16:05:44 +0300

    zalu

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,183 +1,254 @@
-// Запропонуйте користувачу ввести число за допомогою prompt().
-// Збережіть введене значення у змінну number.
-// Перевірте, чи дорівнює змінна number числу 10:
-//    Якщо так – виведіть повідомлення 'Вірно' у alert().
-//    Якщо ні – виведіть 'Невірно'.
+/* const fruits = ["apple", "plum", "pear", "orange"];
 
-// Підказка: prompt завжди повертає рядок (String), тому перед перевіркою
-// перетворіть значення на число за допомогою Number().
-
-/* const number = +prompt("Enter number");
-if (number === 10) {
-  alert("Вірно");
+if (fruits.length >= 3) {
+  console.log("можна поїсти");
 } else {
-  alert("Не вірно");
-} //
-console.log(number); */
+  console.log("ходи голодний");
+} */
 
-// const value = confirm("Are you heare");
-// console.log(value);
-
-// TASK 2
-
-// У змінній min лежить випадкове число від 0 до 59.
-// Визначте, в яку чверть години потрапляє
-// це число (у першу, другу, третю чи четверту).
-// Виведіть в alert відповідне повідомлення, наприклад:
-// "10 входить в першу чверть"
-
-// const min = Math.floor(Math.random() * (59 - 0) + 0);
-/*
-const min = Math.floor(Math.random() * (59 - 0) + 0);
-
-if (min < 15) {
-  alert(min + " в першу чверть");
-} else if (min > 30) {
-  alert(min + " в другу чверть");
-} else if (min > 45) {
-  alert(min + " в третю чверть");
-} else {
-  alert(`${min} в четверту чверть`);
-}
-*/
-
-// task 3
-
-// Змінна num може набувати 4 значення: '1', '2', '3' або '4'
-// (запитуй це значення у користувача через prompt).
-// Якщо вона має значення '1', то у змінну result запишіть 'зима',
-// якщо має значення '2' - 'весна' і так далі.
-// Розв'яжіть завдання через switch-case.
-// Не забудьте про дефолтне значення, на випадок, якщо користувач
-// введе в prompt щось інше. В такому випадку result має набувати значення:
-// "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
-// Значення змінної result виведіть в консоль.
-
-/* const num = prompt("Введіть число від 1 до 4:");
-let result = "";
-
-switch (num) {
-  case "1":
-    result = "зима";
-    break;
-  case "2":
-    result = "весна";
-    break;
-  case "3":
-    result = "літо";
-    break;
-  case "4":
-    result = "осінь";
-    break;
-  default:
-    result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
+/* function getOrderQuantity(order) {
+  return order.length;
 }
 
-console.log(result); */
+console.log(getOrderQuantity(["apple", "peach", "pear", "banana"]));
+console.log(getOrderQuantity(["apple", "banana"]));
+console.log(getOrderQuantity(["apple", "banana", "pear"]));
+console.log(getOrderQuantity([])); */
 
-// task 5
+/* const planets = ["Earth", "Mars", "Venus"];
+const lastElementIndex = planets.length - 1;
+console.log(planets[lastElementIndex]);
+ */
 
-// Напишіть код, який запитуватиме у користувача
-// логін за допомогою prompt і виводить результат в консоль браузера
+/* function getExtremeElements(array) {
+  const firstElement = array[0];
+  const lastElement = array[array.length - 1];
+  return [firstElement, lastElement];
+}
+console.log(getExtremeElements([1, 2, 3, 4, 5]));
+console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
+ */
+/* 
+let a = 5;
 
-// Після цього додайте перевірку введеного користувачем значення:
-// Якщо відвідувач вводить "Адмін",
-// то prompt запитує пароль (не забудьте зберігти його у змінну для подальшої перевірки).
-// Якщо нічого не ввели або натиснули Cancel,
-// Вивести в alert строку "Скасовано"
-// В іншому випадку вивести в alert рядок "Я вас не знаю"
+let b = a;
+console.log(a);
+console.log(b);
+a = 10;
+console.log(a);
+console.log(b);
+ */
 
-// Пароль перевіряти так:
-// Якщо введено пароль "Я головний",
-// то вивести в alert рядок "Добрий день!",
-// в іншому випадку вивести в alert рядок "Невірний пароль!"
+/* const a = ["Mango", "Poly"];
+const b = a;
+console.log(a + 3); // ["Mango", "Poly"]
+console.log(b);
+ */
+/* 
+const emptyArray = [];
+const nonEmptyArray = [1, 2, 3];
 
-/*
+console.log(Boolean(emptyArray)); // true
+console.log(Boolean(nonEmptyArray)); // true
 
-const login = prompt("Введіть логін:");
-console.log("Введений логін:", login);
+if (emptyArray) {
+  console.log("if is in progress");
+} else {
+  console.log("else is not performed");
+}
 
-if (login === "Адмін") {
-  const password = prompt("Введіть пароль:");
+if (nonEmptyArray) {
+  console.log("if is in progress");
+} else {
+  console.log("else is not performed");
+}
+ */
+/* 
+function getLength(array) {
+  return array.join(" ");
+}
 
-  if (password === "Я головний") {
-    alert("Добрий день!");
-  } else if (password === null || password === "") {
-    alert("Скасовано");
-  } else {
-    alert("Невірний пароль!");
+console.log(getLength(["Mango", "hurries", "to", "the", "train"]));
+console.log(getLength(["M", "a", "n", "g", "o"]));
+console.log(getLength(["top", "picks", "for", "you"]));
+ */
+
+/* const name = "Mango";
+const letters = name.split("");
+console.log(letters); // ["M", "a", "n", "g", "o"]
+
+const message = "JavaScript essentials";
+const words = message.split(" ");
+console.log(words); // ["JavaScript", "essentials"]
+
+const slug = "amazing-french-recipes";
+const slugParts = slug.split("-");
+console.log(slugParts); // ["amazing", "french", "recipes"]
+ */
+/* 
+function calculateEngravingPrice(message, pricePerWord) {
+  const words = message.split(" ");
+  return words.length * pricePerWord;
+}
+
+console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
+console.log(calculateEngravingPrice("Web-development is creative work", 40));
+console.log(calculateEngravingPrice("Web-development is creative work", 20));
+ */
+
+/* const fruits = ["apple", "plum", "pear", "orange", "banana"];
+
+const firstTwoEls = fruits.slice(0, 2);
+const nonExtremeEls = fruits.slice(1, 4);
+const lastThreeEls = fruits.slice(-3);
+
+console.log(firstTwoEls);
+console.log(nonExtremeEls);
+console.log(lastThreeEls);
+ */
+/* 
+const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+const newClients = ["Peach", "Houston"];
+const allCilents = oldClients.concat(newClients);
+
+console.log(allCilents);
+ */
+/* 
+const tags = [];
+
+for (let i = 0; i < 3; i += 1) {
+  tags.push(`tag-${i}`);
+}
+
+console.log(tags);
+ */
+
+/* function createArrayOfNumbers(min, max) {
+  const numbers = [];
+
+  for (let i = min; i <= max; i++) {
+    numbers.push(i);
   }
-} else if (login === null || login === "") {
-  alert("Скасовано");
-} else {
-  alert("Я вас не знаю");
-} 
-*/
 
-// task-9
-
-// Напишіть функцію isAdult(age), яка  приймає число - вік користувача і повертає true,
-// якщо параметр age більше чи дорівнює 18.
-// В іншому випадку вона запитує підтвердження через confirm
-// і повертає його результат (true/false).
-/*
-function isAdult(age) {
-  if (age >= 18) {
-    return true;
-  }
-  const answer = confirm("are you adult?");
-  return answer;
+  return numbers;
 }
-console.log(isAdult(10));
-*/
 
-// task 7
+console.log(createArrayOfNumbers(1, 3));
+console.log(createArrayOfNumbers(14, 17));
+console.log(createArrayOfNumbers(29, 34));
+ */
+/* 
+function calculateTotalPrice(order) {
+  let total = 0;
 
-// Напишіть функцію getNumbers(min, max), що приймає 2 параметри -
-// мінімальне і максимальне число відповідно.
-// Напишіть цикл всередині функції, який виводить у консоль
-// всі числа від max до min за спаданням.
-// Окрім цього, підрахуйте суму всіх парних чисел в цьому циклі
-// і поверніть її з функції.
+  for (let i = 0; i < order.length; i++) {
+    total += order[i];
+  }
 
-/*
-function getNumbers(min, max) {
-  let sum = 0;
+  return total;
+}
 
-  for (let i = max; i >= min; i--) {
-    console.log(i);
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+ */
 
+/* function getEvenNumbers(start, end) {
+  const evenNumbers = [];
+
+  for (let i = start; i <= end; i++) {
     if (i % 2 === 0) {
-      sum += i;
+      evenNumbers.push(i);
     }
   }
 
-  return sum;
+  return evenNumbers;
 }
 
-const result = getNumbers(2, 10);
-console.log("Сума парних чисел:", result);
-*/
+console.log(getEvenNumbers(2, 5));
+console.log(getEvenNumbers(3, 11));
+console.log(getEvenNumbers(6, 12));
+console.log(getEvenNumbers(8, 8));
+console.log(getEvenNumbers(7, 7)); */
 
-// task 10
+/* function checkStorage(storage, item) {
+  const lowercasedItem = item.toLowerCase();
 
-// Напишіть функцію fizzBuzz(num), яка приймає число і перевіряє кожне число від 1 до num:
-// Якщо число ділитися  без остачі на 3 - виводить в консоль 'fizz',
-// якщо ділиться  без остачі на 5 - виводить в консоль 'buzz',
-// якщо ділиться  без остачі і на 3, і на 5 - виводить в консоль 'fizzbuzz'.
-
-function fizzBuzz(num) {
-  for (let i = 1; i <= num; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("fizzbuzz");
-    } else if (i % 3 === 0) {
-      console.log("fizz");
-    } else if (i % 5 === 0) {
-      console.log("buzz");
-    } else {
-      console.log(i);
+  for (let i = 0; i < storage.length; i++) {
+    if (storage[i].toLowerCase() === lowercasedItem) {
+      return `${storage[i]} is available to order!`;
     }
   }
+
+  return "Sorry! We are out of stock!";
 }
 
-fizzBuzz(10);
+console.log(checkStorage(["apple", "plum", "pear"], "plum"));
+console.log(checkStorage(["apple", "plum", "pear"], "pLuM"));
+console.log(checkStorage(["apple", "plum", "pear"], "pear"));
+console.log(checkStorage(["apple", "plum", "pear"], "pEAr"));
+console.log(checkStorage(["apple", "plum", "pear"], "orange"));
+console.log(checkStorage(["apple", "plum", "pear"], "carrot"));
+ */
+
+/* function getCommonElements(array1, array2) {
+  const commonElements = [];
+
+  for (let i = 0; i < array1.length; i++) {
+    const element = array1[i];
+
+    if (array2.includes(element)) {
+      commonElements.push(element);
+    }
+  }
+
+  return commonElements;
+}
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+ */
+
+/* function calculateTotalPrice(order) {
+  let total = 0;
+
+  for (const number of order) {
+    total += number;
+  }
+
+  return total;
+}
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+console.log(calculateTotalPrice([]));
+ */
+
+/* function createReversedArray(...args) {
+  return args.toReversed();
+}
+
+console.log(createReversedArray(12, 85, 37, 4));
+console.log(createReversedArray(164, 48, 291));
+console.log(createReversedArray(412, 371, 94, 63, 176));
+console.log(createReversedArray());
+ */
+
+/* function calculateTax(amount, taxRate = 0.2) {
+  return amount * taxRate;
+}
+console.log(calculateTax(100, 0.1));
+console.log(calculateTax(200, 0.1));
+console.log(calculateTax(100, 0.2));
+console.log(calculateTax(200, 0.2));
+console.log(calculateTax(100, 0.3));
+console.log(calculateTax(200, 0.3));
+console.log(calculateTax(100));
+console.log(calculateTax(200));
+ */
